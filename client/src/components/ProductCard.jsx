a1835896685a340d6d5b7b200a0beab10266b8f8
Kingsley <kingkatandawa@gmail.com>
2023-07-16 17:39:23 +0700

    redux setup and products fetched from api

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -4,7 +4,7 @@ import { StarIcon } from '@chakra-ui/icons'
 import {FiShoppingCart} from 'react-icons/fi'
 import {Link, Link as ReactLink} from 'react-router-dom'
 
-const Rating = ({rating, numReviews}) => {
+const Rating = ({rating, numberOfReviews}) => {
   const {iconSize, setIconSize } = useState('14px');
   return(
     <Flex>
@@ -15,8 +15,8 @@ const Rating = ({rating, numReviews}) => {
         <StarIcon size={iconSize} w={'14px'} color={rating >= 4 ? 'orange.500': 'gray.200'} />
         <StarIcon size={iconSize} w={'14px'} color={rating >= 5 ? 'orange.500': 'gray.200'} />
       </HStack>
-      <Text fontSize={'md'} fontWeight={'bold'} ml={'4px'}>
-        {`${numReviews} ${numReviews == 1 ? 'Review': 'Reviews'}`}
+      <Text fontSize={'md'} fontWeight={'bold'} ml={'20px'}>
+        {`${numberOfReviews} ${numberOfReviews === 1 ? 'Review': 'Reviews'}`}
       </Text>
     </Flex>
   )
@@ -28,14 +28,14 @@ const ProductCard = ({product}) => {
         p={2}
         spacing={'3px'}
         bg={useColorModeValue('white','gray.800')}
-        minW={'240px'}
+        minW={'250px'}
         h={'450px'}
         borderWidth={'1px'}
         rounded={'lg'}
         shadow={'lg'}
         position={'relative'}
     >
-        {product.isNew && <Circle size={'10px'} position={'absolute'} top={2} right={2} bg={'green.500'}/> }
+        {product.productIsNew && <Circle size={'10px'} position={'absolute'} top={2} right={2} bg={'green.500'}/> }
         {product.stock < 1 && <Circle size={'10px'} position={'absolute'} top={2} right={2} bg={'red.500'}/> }
         <Image src={product.image} alt={product.name} roundedTop={'lg'} />
         
@@ -45,13 +45,13 @@ const ProductCard = ({product}) => {
               Sold out
             </Badge>
           )}
-          {product.isNew && (
+          {product.productIsNew && (
             <Badge rounded={'full'} px={'2'} fontSize={'0.8em'} colorScheme='green'>
               New
             </Badge>
           )}
         </Box>
-        <Flex mt={'1'} justifyContent={'space-between'} alignContent={'center'}>
+        <Flex mt={'1'} justifyContent={'space-between'} alignContent={'center'} h='80px'>
           <Link as={ReactLink} to={`/product${product.id}`} pt={'2'} cursor={'pointer'}>
             <Box fontSize={'2xl'} fontWeight={'semibold'} lineHeight={'tight'}>
               {product.name}
@@ -60,10 +60,10 @@ const ProductCard = ({product}) => {
         </Flex>
 
         <Flex justifyContent={'space-between'} alignContent={'center'} py={'2'}>
-            <Rating rating={product.rating} numReviews={product.numReviews} />
+            <Rating rating={product.rating} numberOfReviews={product.numberOfReviews} />
         </Flex>
 
-        <Flex mt={'2'} justify={'space-between'}>
+        <Flex mt={'2'} justify={'space-between'} alignContent={'center'}>
           <Box fontSize={'lg'} color={useColorModeValue('gray.800', 'white')}>
             <Box as={'span'} color={'gray.600'} fontSize={'lg'}>
               $
@@ -72,7 +72,7 @@ const ProductCard = ({product}) => {
           </Box>
           <Tooltip label='Add to cart' bg='white' placement='top' color={'gray.800'} fontSize={'1.2em'}>
             <Button variant={'ghost'} display={'flex'} disabled={product.stock <=0}>
-              <Icon as={FiShoppingCart} h={7} w={7} alignSelf={'center'} />
+              <Icon as={FiShoppingCart} h={5} w={5} alignSelf={'center'} />
             </Button>
           </Tooltip>
         </Flex>
--- client/src/screens/ProductsScreen.jsx
+++ client/src/screens/ProductsScreen.jsx
@@ -1,16 +1,43 @@
-import { Center, Wrap, WrapItem } from '@chakra-ui/react'
-import { products } from '../products'
-import ProductCard from '../components/ProductCard'
+import { Alert, AlertDescription, AlertIcon, AlertTitle, Center, Spinner, Stack, Wrap, WrapItem } from '@chakra-ui/react'
+import ProductCard from '../components/ProductCard';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getProducts } from '../redux/actions/productActions';
+
+
 const ProductsScreen = () => {
+
+  const dispatch = useDispatch();
+  console.log(dispatch)
+
+  const {loading, error, products} = useSelector(state => state.products)
+
+  useEffect(() => {
+    dispatch(getProducts())
+  }, [dispatch])
+
   return (
     <Wrap spacing={'30px'} justify={'center'} minHeight={'100vh'}>
-        {products.map((product) => (
-            <WrapItem key={product._id}>
-                <Center w={'250px'} h={'550px'}>
-                    <ProductCard product={product} />
-                </Center>
-            </WrapItem>
-        ))}
+      {loading ? (
+        <Stack direction={'row'} spacing={4}>
+          <Spinner mt={20} thickness='2px' speed='0.65s' emptyColor='gray.200' color='orange.200' size={'xl'} />
+        </Stack>
+      ): error ? (
+        <Alert status='error'>
+          <AlertIcon />
+          <AlertTitle>We are sorry!</AlertTitle>
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      ):(
+        products.map((product) => (
+          <WrapItem key={product._id}>
+              <Center w={'250px'} h={'550px'}>
+                  <ProductCard product={product} />
+              </Center>
+          </WrapItem>
+        ))
+        ) 
+        }
     </Wrap>
   )
 }
